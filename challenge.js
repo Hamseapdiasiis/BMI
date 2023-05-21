dbae3f2c0e026478ce07fcd5ef08cb2c2b9724f2
Hamseapdiasiis <hamse906@gmail.com>
2023-05-21 17:55:11 +0300

    Exercise done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function BMI(weight,hieght){
+    return weight/(hieght*hieght)
+}
+let Ali = BMI(69,1.75)
+let mohamed = BMI(89,1.70)
+
+if(Ali > mohamed){
+    console.log("Ali ayaa ka buuran Mohamed")
+}else{
+    console.log("Mohamed ayaa ka buuran Ali")
+}
